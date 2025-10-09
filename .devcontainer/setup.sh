6bbb7726e5f779e57a6fcb689653f120124fd4ff
Sn0w3W <57131742+Sn0w3W@users.noreply.github.com>
2025-10-09 20:27:39 +0000

    Solution to bluetooth problems on k3v2

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -15,3 +15,23 @@ git config --global --add safe.directory "$userspacePath"
 git submodule update --init --recursive
 
 qmk git-submodule
+
+TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-aarch64-linux.tar.bz2"
+INSTALL_DIR="/opt/arm-gnu-toolchain"
+BIN_DIR="$INSTALL_DIR/bin"
+
+echo "[*] Installing ARM GNU Toolchain..."
+mkdir -p "$INSTALL_DIR"
+cd /tmp
+
+wget -q --show-progress -O toolchain.tar.bz2 "$TOOLCHAIN_URL"
+tar -xjf toolchain.tar.bz2 -C "$INSTALL_DIR" --strip-components=1
+rm toolchain.tar.bz2
+
+echo "export PATH=\"$BIN_DIR:\$PATH\"" >> /root/.bashrc
+export PATH="$BIN_DIR:$PATH"
+
+echo "[*] Installed version:"
+arm-none-eabi-gcc --version | head -n 1
+
+echo "[✔] ARM GCC successfully installed."
