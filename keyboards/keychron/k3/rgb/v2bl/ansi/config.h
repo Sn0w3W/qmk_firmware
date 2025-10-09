a9605d0d21fcb5d5c4830f349097a42e678d423d
Sn0w3W <57131742+Sn0w3W@users.noreply.github.com>
2025-10-09 20:27:39 +0000

    Solution to bluetooth problems on k3v2

--- keyboards/keychron/k3/rgb/v2bl/ansi/config.h
+++ keyboards/keychron/k3/rgb/v2bl/ansi/config.h
@@ -15,22 +15,3 @@
  */
 
 #define RGB_MATRIX_LED_COUNT 84
-
-// Measures to cope with phantom column/key noise.
-// Uncomment the lines below if the ` or Tab keys press themselves in Bluetooth mode.
-
-// 20–50 is a reasonable range. Increase this value if the keyboard both spams and stops working when RGB is turned off.
-// #define MATRIX_IO_DELAY 50
-
-// 20–50 is a reasonable range. Increase this value if the keyboard both spams and stops working when RGB is turned off.
-// #define MATRIX_UNSELECT_DRIVE_DELAY 50
-
-// 1000–2000 is generally optimal based on testing.
-// #define MATRIX_SCAN_RATE 1000
-
-// Per-key debounce type. If you change the debounce mode, you may need to retune the delay value below.
-// #define DEBOUNCE_TYPE sym_defer_pk
-
-// Debounce time in milliseconds. Increasing this value too much may cause the keyboard
-// to stop responding at low RGB brightness or fail to register keypresses in Bluetooth mode.
-// #define DEBOUNCE 8
