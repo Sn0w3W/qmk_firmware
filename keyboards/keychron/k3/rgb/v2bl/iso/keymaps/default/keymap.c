7558bab7aa50b3caf7944254c1f563d7d65a4923
Sn0w3W <57131742+Sn0w3W@users.noreply.github.com>
2025-10-09 16:40:55 +0000

    Adding support for bluetooth on Keychron K3v2, both for ANSI and ISO variants.

--- keyboards/keychron/k3/rgb/v2bl/iso/keymaps/default/keymap.c
+++ keyboards/keychron/k3/rgb/v2bl/iso/keymaps/default/keymap.c
@@ -1,42 +1,3 @@
-/*
-Copyright 2020 Dimitris Mantzouranis
-Copyright 2022 Pablo Ramirez
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-#include QMK_KEYBOARD_H
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-enum layer_names {
-  MAC_BASE = 0,
-  MAC_FN = 1,
-  WIN_BASE = 2,
-  WIN_FN = 3
-};
-
-#define KC_TASK LGUI(KC_TAB)        // Task viewer
-#define KC_FLXP LGUI(KC_E)          // Windows file explorer
-#define KC_SNIP LGUI(LSFT(KC_S))    // Windows snip tool
-
-#define KC_MSSN 0xc1                // Mission Control
-#define KC_LAUN 0xc2                // Launchpad
-#define KC_MSCR LSFT(LGUI(KC_3))    // Mac screenshot
-#define KC_MSNP LSFT(LALT(KC_4))    // Mac snip tool
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /*  Mac base layout
@@ -139,34 +100,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,    _______,    _______,                                        _______,                                        _______,    _______,    _______,    RM_HUED,    RM_SATD,    RM_HUEU
   )
 };
-
-bool dip_switch_update_user(uint8_t index, bool active) {
-  switch(index) {
-    case 0: // Connection switch
-    // Probably it's not possible to do anything sensible here as switching from Cable to BT requires turning off the board. (BT / OFF / Cable)
-      if (active) { //Cable mode
-
-      }
-      else { // BT mode
-
-      }
-      break;
-    case 1: // OS switch
-      if (active) { // iOS / macOS mode
-        layer_move(0);
-      }
-      else { // Windows mode
-        layer_move(2);
-      }
-      break;
-  }
-  return true;
-}
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  // debug_enable = true;
-  // debug_matrix = true;
-  // debug_keyboard = true;
-  // debug_mouse = true;
-}
